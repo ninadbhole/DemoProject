2cb76fce5f6e22c6b8dda4e4ef6bd0ea24b088de
Ninad Bhole <ninad.bhole@plansource.com>
2023-03-14 23:12:07 +0530

    Added WatchList Model and registerd it with admin.py

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from watchlist_app.models import WatchList
 
-# Register your models here.
+admin.site.register(WatchList)
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class WatchList(models.Model):
+    title = models.CharField(max_length=100)
+    storyline = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.title
